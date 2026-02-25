f8921a778f377b96518bbe9998827769b2d19031
Augusto Saboia <134610628+Augustuuuuu@users.noreply.github.com>
2026-02-25 18:38:17 -0300

    Update create_admin.py

--- create_admin.py
+++ create_admin.py
@@ -7,8 +7,17 @@ django.setup()
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@exemplo.com', 'SenhaForte123')
-    print("Usuário administrador criado: admin / SenhaForte123")
+username = 'admin'
+email = 'admin@exemplo.com'
+password = 'SuaSenhaForte123' # Verifique se digitou esta exatamente assim
+
+user, created = User.objects.get_or_create(username=username, defaults={'email': email})
+
+if created:
+    user.set_password(password)
+    user.save()
+    print(f"Usuário {username} criado com sucesso!")
 else:
-    print("Usuário administrador já existe.")
+    user.set_password(password) # Força a atualização da senha se ele já existir
+    user.save()
+    print(f"Usuário {username} já existia, senha atualizada!")
